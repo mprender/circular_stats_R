4b071555392ed7877467a0a7ca394abc8d8dca61
mprender <meprender@gmail.com>
2019-04-26 22:21:58 -0400

    Update install.R

--- install.R
+++ install.R
@@ -2,4 +2,4 @@ install.packages("ggplot2")
 install.packages("stats")
 install.packages("circular", repos="http://R-Forge.R-project.org")
 install.packages("dplyr")
-
+install.packages("r-cran-circular")
