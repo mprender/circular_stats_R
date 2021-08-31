f44d8fa20d84595ccd093dd40fa13ca65b610279
mprender <meprender@gmail.com>
2021-08-31 11:41:31 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,4 +3,10 @@
 #install.packages("ggplot2", dependencies = TRUE)
 #install.packages("circular")
 
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+
 
