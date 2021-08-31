7a6df8902ca149fad25a01041692ff77e851ed12
mprender <meprender@gmail.com>
2021-08-31 11:38:58 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
-install.packages("tidyverse")
-install.packages("rlang")
-install.packages("ggplot2", dependencies = TRUE)
-install.packages("circular")
+#install.packages("tidyverse")
+#install.packages("rlang")
+#install.packages("ggplot2", dependencies = TRUE)
+#install.packages("circular")
 
 
